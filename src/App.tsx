a884658e9ccb9ac425bf3e042250991c979c2139
ThVinicius <vinicioss9955@gmail.com>
2022-12-10 17:54:14 -0300

    feat: react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ResetCSS from './assets/css/Reset'
+import GlobalCSS from './assets/css/Global'
+import Header from './components/header/Header'
+import Home from './pages/home/Home'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <ResetCSS />
+      <GlobalCSS />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
