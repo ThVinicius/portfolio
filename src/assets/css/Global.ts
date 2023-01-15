7bf40b7b1ebb8211b7abc75774caa88f527f981c
ThVinicius <vinicioss9955@gmail.com>
2023-01-15 18:17:25 -0300

    style: new font

--- src/assets/css/Global.ts
+++ src/assets/css/Global.ts
@@ -9,6 +9,18 @@ const GlobalCSS = createGlobalStyle`
     text-decoration: none;
   }
 
+  @font-face {
+  font-family: "Grotesque";
+  src: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/397014/BrandonGrotesque-Regular.eot"), url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/397014/BrandonGrotesque-Regular.ttf"), url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/397014/BrandonGrotesque-Regular.woff");
+  font-weight: normal;
+}
+
+@font-face {
+  font-family: "Grotesque Black";
+  src: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/397014/BrandonGrotesque-Black.eot"), url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/397014/BrandonGrotesque-Black.ttf"), url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/397014/BrandonGrotesque-Black.woff");
+  font-weight: bold;
+}
+
 `
 
 export default GlobalCSS
