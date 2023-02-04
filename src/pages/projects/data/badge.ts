22d2c5ebe77e1ed3fbd916f762c9250db9f6c331
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-02-04 20:33:14 -0300

    feat:  new badge

--- src/pages/projects/data/badge.ts
+++ src/pages/projects/data/badge.ts
@@ -17,5 +17,7 @@ export const badge = {
     'https://img.shields.io/badge/-cypress-%23E5E5E5?style=for-the-badge&logo=cypress&logoColor=058a5e',
   java: 'https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white',
   spring:
-    'https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white'
+    'https://img.shields.io/badge/spring-%236DB33F.svg?style=for-the-badge&logo=spring&logoColor=white',
+  docker:
+    'https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white'
 }
