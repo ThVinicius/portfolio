76c3161f8418060738902427cb744e9117e61354
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-04-24 17:39:42 -0300

    feat:  main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <main>Home</main>
+import Image from 'next/image'
+import Link from 'next/link'
+import { FC } from 'react'
+
+import dev from '../../public/assets/images/dev.gif'
+import S from './home.module.scss'
+
+const Content: FC = () => {
+  return (
+    <main className={S.container}>
+      <div className={S['container-text']}>
+        <h2>Vinicius Pacheco dos Santos</h2>
+        <h1>Desenvolvedor Web Full Stack</h1>
+        <Link
+          className={S.link}
+          href="/assets/resume/Vinicius_Pacheco dos Santos_Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button className={S.button}>Download CV</button>
+        </Link>
+      </div>
+      <div className={S.box}>
+        <Image className={S.image} src={dev} alt="dev gif" />
+      </div>
+    </main>
+  )
 }
+
+export default Content
