22129f6990ad06e2ceb68001add14dd4040aaf93
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-04-29 10:53:26 -0300

    build: image domains

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true
   },
+  images: {
+    domains: ['img.shields.io', 'imgcloud.com.br'],
+    dangerouslyAllowSVG: true
+  }
 }
 
 module.exports = nextConfig
