50b460a8c3a0178e4b4b08976ae9cf0e0fd8d4a2
Vinicius Pacheco dos Santos <vinicioss9955@gmail.com>
2023-05-04 10:46:35 -0300

    feat: remove created_at and updated_at

--- src/types/projects.ts
+++ src/types/projects.ts
@@ -5,14 +5,6 @@ export type IProject = {
   description: string
   image: string
   position: number | null
-  badges: { name: string; image: string; created_at: Date; updated_at: Date }[]
-  links: {
-    id: number
-    label: string
-    href: string
-    created_at: Date
-    updated_at: Date
-  }[]
-  created_at: Date
-  updated_at: Date
+  badges: { name: string; image: string }[]
+  links: { id: number; label: string; href: string }[]
 }
